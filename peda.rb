26eaad80cee40619ce91d1e809ac4a92a885db10
Héctor Salazar <h.salazar509@gmail.com>
2015-10-29 10:39:51 -0600

    Agregado método privado 'is_wknd' y argumentos desde la terminal

--- peda.rb
+++ peda.rb
@@ -6,16 +6,31 @@ class Peda
     @n_amigos = n_amigos
     @time = time
   end
-  
+
   def armar
-    if @time.wday == 6 and @n_pomos > 0 and @n_amigos > 0 # wday = 6 => saturday
+    if is_wknd?(@time) and @n_pomos > 0 and @n_amigos > 0 # wday = 6 => saturday
       "ya se armo este pex"
     else
       "ya se chingo este pex"
     end
   end
 
+  private
+  def is_wknd?(time)
+    time.friday? or time.saturday?
+  end
+
 end
 
-laPeda = Peda.new(10000, 5, Time.now) # depending on system time
+# Lee las variables desde el comando
+args = Hash[ ARGV.join(' ').scan(/--?([^=\s]+)(?:=(\S+))?/) ]
+
+# USAGE
+#
+# ruby peda.rb --pomos=2 --amigos=7
+
+pomos = args['pomos'].to_i
+amigos = args['amigos'].to_i
+
+laPeda = Peda.new(pomos, amigos, Time.now) # depending on system time
 puts laPeda.armar
